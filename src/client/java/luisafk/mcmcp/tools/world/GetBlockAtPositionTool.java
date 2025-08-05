065caf8c1102598499f8073b92d935d0eaaa952d
lafkpages <luisafk@proton.me>
2025-08-05 17:41:56 +0900

    feat: add GetBlockAtPositionTool

---
+++ src/client/java/luisafk/mcmcp/tools/world/GetBlockAtPositionTool.java
@@ -0,0 +1,75 @@
+package luisafk.mcmcp.tools.world;
+
+import static luisafk.mcmcp.Client.MC;
+
+import java.util.Map;
+
+import io.modelcontextprotocol.server.McpServerFeatures;
+import io.modelcontextprotocol.spec.McpSchema.CallToolResult;
+import io.modelcontextprotocol.spec.McpSchema.Tool;
+import luisafk.mcmcp.tools.BaseTool;
+import net.minecraft.block.BlockState;
+import net.minecraft.util.math.BlockPos;
+
+public class GetBlockAtPositionTool extends BaseTool {
+
+    private static final String SCHEMA = """
+            {
+                "type": "object",
+                "properties": {
+                    "x": {
+                        "type": "number",
+                        "description": "X coordinate of the block position"
+                    },
+                    "y": {
+                        "type": "number",
+                        "description": "Y coordinate of the block position"
+                    },
+                    "z": {
+                        "type": "number",
+                        "description": "Z coordinate of the block position"
+                    }
+                },
+                "required": ["x", "y", "z"]
+            }
+            """;
+
+    @Override
+    public McpServerFeatures.SyncToolSpecification create() {
+        return new McpServerFeatures.SyncToolSpecification(
+                new Tool("get_block_at_position", "Get information about the block at a specific position", SCHEMA),
+                this::execute);
+    }
+
+    private CallToolResult execute(Object exchange, Map<String, Object> arguments) {
+        if (!isWorldAvailable()) {
+            return worldNotFoundError();
+        }
+
+        double x = ((Number) arguments.get("x")).doubleValue();
+        double y = ((Number) arguments.get("y")).doubleValue();
+        double z = ((Number) arguments.get("z")).doubleValue();
+
+        BlockPos pos = new BlockPos((int) Math.floor(x), (int) Math.floor(y), (int) Math.floor(z));
+        BlockState blockState = MC.world.getBlockState(pos);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format("Block at position [%d, %d, %d]:\n", pos.getX(), pos.getY(), pos.getZ()));
+        sb.append(String.format("Block type: %s\n", blockState.getBlock().toString()));
+        sb.append(String.format("Block state: %s\n", blockState.toString()));
+        sb.append(String.format("Display name: %s\n", blockState.getBlock().getName().getString()));
+
+        // Additional block properties
+        sb.append(String.format("Is air: %s\n", blockState.isAir()));
+        sb.append(String.format("Is solid: %s\n", blockState.isSolidBlock(MC.world, pos)));
+        sb.append(String.format("Is transparent: %s\n", !blockState.isOpaque()));
+        sb.append(String.format("Hardness: %.1f\n", blockState.getHardness(MC.world, pos)));
+
+        // Light properties
+        sb.append(String.format("Light level: %d\n", MC.world.getLightLevel(pos)));
+        sb.append(String.format("Block light: %d\n", MC.world.getLightLevel(net.minecraft.world.LightType.BLOCK, pos)));
+        sb.append(String.format("Sky light: %d", MC.world.getLightLevel(net.minecraft.world.LightType.SKY, pos)));
+
+        return new CallToolResult(sb.toString(), false);
+    }
+}
