b90b0c24edfc33a360f5d714a595c9126f9351f5
lafkpages <luisafk@proton.me>
2025-08-09 21:08:50 -0700

    feat: add basic YACL screen and Mod Menu integration

---
+++ src/client/java/luisafk/mcmcp/config/ModMenuIntegration.java
@@ -0,0 +1,15 @@
+package luisafk.mcmcp.config;
+
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+
+import dev.isxander.yacl3.api.YetAnotherConfigLib;
+
+public class ModMenuIntegration implements ModMenuApi {
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return parentScreen -> YetAnotherConfigLib.createBuilder()
+                .build()
+                .generateScreen(parentScreen);
+    }
+}
